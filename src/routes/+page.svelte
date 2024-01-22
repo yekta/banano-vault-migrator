e39d9ae757bc5d17a49af6411016b11064e05492
yekta <ngyekta@gmail.com>
2024-01-23 02:32:58 +0300

    Log decrypted seed in the console

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,6 +64,7 @@
 				password: passwordInput,
 				encryptedSeed
 			});
+			console.log(`Decrypted seed is: ${_decryptedSeed}`);
 			if (!_decryptedSeed) {
 				passwordInputError = 'Something went wrong with the decryption.';
 				return;
